349dddd96b73d3e96c878aef667e416bcc77c280
James Jiang <jamesjiang52@gmail.com>
2018-01-10 23:11:37 -0500

    Add day 18

---
+++ Day 18 Part 2.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 28 12:47:37 2017
+
+@author: James Jiang
+"""
+
+string = ''
+with open('Day 18 Data.txt') as f:
+    for line in f:
+        string = line
+
+def is_safe(index, row):
+    row = '.' + row + '.'
+    index = index + 1
+    if row[index - 1] == row[index + 1]:
+        return True
+    else:
+        return False
+
+string_list = [i for i in string]       
+total = string_list.count('.')
+
+for i in range(399999):
+    new_row = ''
+    for j in range(len(string)):
+        if is_safe(j, string) == True:
+            new_row += '.'
+            total += 1
+        else:
+            new_row += '^'
+    string = new_row
+
+print(total)
