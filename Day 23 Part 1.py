b0c1b9fe41df96d43a111b727b5cad0f07ed82e1
James Jiang <jamesjiang52@gmail.com>
2018-01-14 21:48:41 -0500

    Add day 23

---
+++ Day 23 Part 1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 30 13:36:20 2017
+
+@author: James Jiang
+"""
+
+multiply_c = 76
+multiply_d = 84
+
+a = 7
+b = a
+
+for i in range(1, b):
+    a *= i
+    
+a += multiply_c*multiply_d
+print(a)
